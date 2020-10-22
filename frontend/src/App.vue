a9801740833a70dab761d67d69ed50735c8163c1
Pomegranate123 <elias@hoste.info>
2020-10-22 20:45:28 +0200

    Add menu bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <nav>
+      <ul>
+        <router-link tag="li" id="button" to="/">Home</router-link>
+        <router-link tag="li" id="button" to="/rang">Rang</router-link> 
+        <router-link tag="li" id="button" to="/klas">Klas</router-link> 
+        <router-link tag="li" id="button" to="/winkel">Winkel</router-link> 
+        <router-link tag="li" id="button" to="/profiel">Profiel</router-link> 
+        <router-link tag="li" id="button" to="/opties">Opties</router-link>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <router-view/>
+  </main>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +23,45 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#button {
+  display: inline-block;
+  margin: 0.5em 0;
+  padding: 1em 2em;
+  font-size: 1em;
+  font-weight: 700;
+  letter-spacing: 0.02em;
+  line-height: 1;
+  text-decoration: none;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+#button:hover {
+  color: #ffffff;
+}
+
+nav {
+  position: fixed;
+  height: 100px;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  background: rgb(53, 53, 53);
+}
+
+nav a {
+  font-weight: bold;
+  color: #888888;
+}
+
+nav a.router-link-exact-active {
+  color: #c43939;
+  cursor: default;
+}
+
+main {
+  margin-top: 100px;
 }
 </style>
