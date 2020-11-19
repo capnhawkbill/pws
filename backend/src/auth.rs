613f41719a35f86ee8c6e27f57dd1e6d02f5deaa
capnhawkbill <capnhawkbill@airmail.cc>
2020-11-19 10:14:48 +0100

    Requestguards for basic auth

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -1,80 +1,171 @@
+use anyhow::Result;
+use rocket::request::{self, Request, FromRequest, Outcome};
 use rocket::http::Status;
-use rocket::request::{self, FromRequest, Request};
-use rocket::{Outcome, State};
-use serde::{Deserialize, Serialize};
-use crate::config::AppState;
 
-use jsonwebtoken as jwt;
+use super::models;
 
-<<<<<<< HEAD
-/// A request guard for JWT authentication
-=======
-use crate::config;
+// TODO Nonsense
+type Database = bool;
 
->>>>>>> 3f190f961927985159b262e44c1ad6ed0b25df13
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Auth {
-    /// timestamp
-    pub exp: i64,
-    /// user id
-    pub id: i32,
-    pub username: String,
+/// This is a request guard for logging in as any user
+pub enum User {
+    Student(models::Student),
+    Teacher(models::Teacher),
+    Admin(models::Admin),
 }
 
-impl Auth {
-<<<<<<< HEAD
-    /// Generate a token for the Auth
-    pub fn token(&self, secret: &jwt::EncodingKey) -> String {
-        jwt::encode(&jwt::Header::default(), self, &secret).expect("jwt")
-=======
-    pub fn token(&self, secret: &[u8]) -> String {
-        jwt::encode(&jwt::Header::default(), self, secret).expect("jwt")
->>>>>>> 3f190f961927985159b262e44c1ad6ed0b25df13
-    }
+
+/// This is a request guard for logging in as a user
+pub struct Student {}
+
+/// This is a request guard for logging in as a teacher
+pub struct Teacher {}
+
+/// This is a request guard for logging in as a admin
+pub struct Admin {}
+
+/// The error type for logging in
+#[derive(Debug)]
+pub enum LoginError {
+    /// Invalid password or username
+    Invalid,
+    /// Database error
+    Database,
+    /// Invalid format
+    Format,
+    /// Password or username missing
+    Missing,
+    /// Invalid permissions
+    Permission,
 }
 
-impl<'a, 'r> FromRequest<'a, 'r> for Auth {
-    type Error = ();
+/// Verify that the user is an admin
+impl<'a, 'r> FromRequest<'a, 'r> for Admin {
+    type Error = LoginError;
+    fn from_request(req: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        // TODO get the database
+        //let db = req.guard::
+        let db = true;
 
-    /// Extract Auth token from the "Authorization" header.
-    ///
-    /// Handlers with Auth guard will fail with 503 error.
-    /// Handlers with Option<Auth> will be called with None.
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Auth, Self::Error> {
-        let state: State<AppState> = request.guard()?;
-        if let Some(auth) = extract_auth_from_request(request, &state.secret) {
-            Outcome::Success(auth)
-        } else {
-            Outcome::Failure((Status::Forbidden, ()))
+        // Retrieve header
+        let header: Vec<_> = req.headers().get("Authorization").collect();
+        // Check for the correct amount
+        if header < 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Missing))
+        } else if header > 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Format))
+        }
+
+        let user = check_value(header, db).map_err(|err|{
+            Outcome::Failure((Status::BadRequest, err))
+        })?;
+
+        match user {
+            User::Admin(a) => Outcome::Success(a),
+            _ => Outcome::Failure((Status::BadRequest, LoginError::Permission))
         }
     }
 }
 
-fn extract_auth_from_request(request: &Request, secret: &[u8]) -> Option<Auth> {
-    request
-        .headers()
-        .get_one("authorization")
-        .and_then(extract_token_from_header)
-        .and_then(|token| decode_token(token, secret))
-}
+/// Verify that the user is a teacher
+impl<'a, 'r> FromRequest<'a, 'r> for Teacher {
+    type Error = LoginError;
+    fn from_request(req: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        // TODO get the database
+        //let db = req.guard::
+        let db = true;
 
-fn extract_token_from_header(header: &str) -> Option<&str> {
-    if header.starts_with(config::TOKEN_PREFIX) {
-        Some(&header[config::TOKEN_PREFIX.len()..])
-    } else {
-        None
+        // Retrieve header
+        let header: Vec<_> = req.headers().get("Authorization").collect();
+        // Check for the correct amount
+        if header < 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Missing))
+        } else if header > 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Format))
+        }
+
+        let user = check_value(header, db).map_err(|err|{
+            Outcome::Failure((Status::BadRequest, err))
+        })?;
+
+        match user {
+            User::Teacher(t) => Outcome::Success(t),
+            _ => Outcome::Failure((Status::BadRequest, LoginError::Permission))
+        }
     }
 }
 
-/// Decode token into `Auth` struct. If any error is encountered, log it
-/// an return None.
-fn decode_token(token: &str, secret: &[u8]) -> Option<Auth> {
-    use jwt::{Algorithm, Validation};
+/// Verify that the user is a student
+impl<'a, 'r> FromRequest<'a, 'r> for Student {
+    type Error = LoginError;
+    fn from_request(req: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        // TODO get the database
+        //let db = req.guard::
+        let db = true;
 
-    jwt::decode(token, secret, &Validation::new(Algorithm::HS256))
-        .map_err(|err| {
-            eprintln!("Auth decode error: {:?}", err);
-        })
-        .ok()
-        .map(|token_data| token_data.claims)
+        // Retrieve header
+        let header: Vec<_> = req.headers().get("Authorization").collect();
+        // Check for the correct amount
+        if header < 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Missing))
+        } else if header > 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Format))
+        }
+
+        let user = check_value(header, db).map_err(|err|{
+            Outcome::Failure((Status::BadRequest, err))
+        })?;
+
+        match user {
+            User::Student(s) => Outcome::Success(s),
+            _ => Outcome::Failure((Status::BadRequest, LoginError::Permission))
+        }
+    }
+}
+
+/// Verify that the user is authenticated
+impl<'a, 'r> FromRequest<'a, 'r> for User {
+    type Error = LoginError;
+    fn from_request(req: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        // TODO get the database
+        //let db = req.guard::
+        let db = true;
+
+        // Retrieve header
+        let header: Vec<_> = req.headers().get("Authorization").collect();
+        // Check for the correct amount
+        if header < 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Missing))
+        } else if header > 1 {
+            return Outcome::Failure((Status::BadRequest, LoginError::Format))
+        }
+
+        let user = check_value(header, db).map_err(|err|{
+            Outcome::Failure((Status::BadRequest, err))
+        })?;
+
+        Outcome::Success(user)
+
+    }
+}
+
+/// Verify a base64 encoded username and password pair
+/// It should be in the format "username:password"
+// TODO it currently gets the whole header i don't know if this is a problem
+pub fn check_value(value: &[u8], db: Database) -> std::result::Result<User, LoginError> {
+    let value = base64::decode(value).split(|x| x == ':');
+
+    if value.clone().count() != 2 {
+        return Err(LoginError::Format)
+    }
+
+    // Unwraps should be save because of the check above
+    let (username, password) = (value.next().unwrap(), value.next().unwrap());
+
+    // TODO Get the stuff from the database
+
+    // TODO Check them against eachother
+    // TODO Return the correct variant
+
+    todo![];
 }
---
+++ backend/todo.md
@@ -0,0 +1,9 @@
+# Backend
+
+## Models refractor
+- Class, teacher, admin, student
+- Nadenken over hoe teachers en students bij de class horen
+  - [cross reference table](https://en.wikipedia.org/wiki/Associative_entity)
+  - class stores users
+  - users store classes
+  - both
