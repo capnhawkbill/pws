36df9b1bdd2d1aaa4b47c7c9c413e2d195a3ba61
capnhawkbill <capnhawkbill@airmail.cc>
2020-11-20 10:27:50 +0100

    no login route needed for basic auth

--- backend/src/routes/login.rs
+++ backend/src/routes/login.rs
@@ -14,30 +14,6 @@ pub struct Credentials {
     password: String,
 }
 
-/// The apikey that is returned as json
-#[derive(Serialize)]
-pub struct ApiKey {
-    apikey: String,
-}
-
-/// Just sends everything to the login function
-#[post("/login", format = "json", data = "<credentials>")]
-pub fn login(conn: UserDataBase, credentials: Json<Credentials>) -> Result<Json<ApiKey>> {
-    todo![]
-    //    let apikey = match login::login(
-    //        &conn,
-    //        (*credentials.username).to_string(),
-    //        (*credentials.password).to_string(),
-    //    ) {
-    //        Ok(v) => v,
-    //        // If the username or password is incorrect return that otherwise error
-    //        Err(e) => match e {
-    //            _ => todo![],
-    //        },
-    //    };
-    //    Ok(Json(ApiKey { apikey: apikey }))
-}
-
 /// Just sends everything to the signup function
 #[post("/signup", format = "json", data = "<credentials>")]
 pub fn signup(conn: UserDataBase, credentials: Json<Credentials>) -> Result<Json<ApiKey>> {
