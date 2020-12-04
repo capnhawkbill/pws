430512722d0086e9c957124fd4b07697bb0dee07
capnhawkbill <capnhawkbill@airmail.cc>
2020-12-04 09:53:05 +0100

    badge database helper functions

--- backend/src/database/mod.rs
+++ backend/src/database/mod.rs
@@ -3,4 +3,4 @@ use rocket_contrib::databases::rustqlite;
 pub mod models;
 
 #[database("sqlite_logs")]
-pub struct DbConn(rustqlite::Connection);
+pub struct DbConn(rusqlite::Connection);
--- backend/src/database/models/badge.rs
+++ backend/src/database/models/badge.rs
@@ -1,4 +1,5 @@
 use super::Id;
+use rusqlite::Connection;
 
 /// A badge that cab be awarded to students
 pub struct Badge {
@@ -23,3 +24,30 @@ pub enum Condition {}
 impl std::fmt::Display for Condition {}
 // TODO fromstr condition
 impl FromStr for Condition {}
+
+fn insert_badge(conn: Connection, badge: Badge) -> Result<()> {
+    conn.execute(
+        "INSERT INTO badge (id, name, description, official, condition)",
+        params![badge.id, badge.name, badge.description, badge.official, badge.condition]
+    )?;
+    Ok(())
+}
+
+fn get_badge(conn: Connection, id: Id) -> Result<Badge> {
+    let stmt = conn.prepare("SELECT * FROM badge where id = :id");
+    let badges = stmt.query_map(params!["id", id], |row| {
+        Ok(Badge {
+            id: row.get(0),
+            name: row.get(1),
+            description: row.get(2),
+            official: row.get(3),
+            condition: row.get(4),
+        })
+    })?;
+
+    if let Some(badge) = badges.next() {
+        Ok(badge)
+    } else {
+        Err(anyhow!("No badge found"))
+    }
+}
