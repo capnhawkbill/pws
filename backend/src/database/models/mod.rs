1c336ae0ccb4af6c5cc3c63e3deddfe97380cf9c
capnhawkbill <capnhawkbill@airmail.cc>
2020-12-09 14:58:45 +0100

    add badge

--- backend/src/database/models/mod.rs
+++ backend/src/database/models/mod.rs
@@ -5,6 +5,7 @@ mod teacher;
 pub use class::*;
 pub use student::*;
 pub use teacher::*;
+pub use badge::*;
 
 /// Id for types in the database
 /// `varchar(50)`
--- backend/src/database/models/student.rs
+++ backend/src/database/models/student.rs
@@ -1,21 +1,27 @@
 use super::Id;
+use super::Badge;
 
 pub struct Student {
     /// The id of the student
     id: Id,
     /// The name of the student
     name: String,
-    /// The password of the student
+    /// The password of the student TODO plaintext lol
     password: String,
     /// Other information that isn't strictly necessary
     info: Option<StudentInfo>,
     /// Id's of the classes the student is in
     classes: Vec<Id>,
+    badges: Vec<Badge>
 }
 
 /// Non necessary information about a student
 pub struct StudentInfo {
     /// Gender
     gender: Option<String>,
+    /// Status
+    status: Option<String>,
+    /// Disorder
+    disorders: Option<Vec<String>>
     // TODO more unnecessary information
 }
