7c34481a2e002854e48b23df414c76db805997a0
capnhawkbill <capnhawkbill@airmail.cc>
2020-12-27 09:15:44 +0100

    Added teacher functions

--- backend/src/database/models/teacher.rs
+++ backend/src/database/models/teacher.rs
@@ -1,20 +1,56 @@
 use super::Id;
+use anyhow::{anyhow, Result};
+use super::super::{mkcsv, getcsv};
+use rocket_contrib::databases::rusqlite::Connection;
 
 /// The teacher
 pub struct Teacher {
-    /// The id of the student
+    /// The id of the teacher
     id: Id,
-    /// The name of the student
+    /// The name of the teacher
     name: String,
     /// The password of the teacher
     password: String,
-    /// Other information that isn't strictly necessary
-    info: Option<TeacherInfo>,
-    /// Id's of the classes the student is in
+    /// Id's of the classes the teacher is in
     classes: Vec<Id>,
 }
 
-/// Non necessary information about a student
-pub struct TeacherInfo {
-    gender: Option<String>, // TODO more unnecessary information
+/// Insert a teacher into the database
+pub fn insert_teacher(conn: Connection, teacher: Teacher) -> Result<()> {
+    // Convert to csv
+    let classes = mkcsv(&teacher.classes)?;
+    // Convert to json
+    conn.execute(
+            "INSERT INTO teacher (id, name, password, classes) VALUES (?1, ?2, ?3, ?45)",
+            &[&teacher.id, &teacher.name, &teacher.password, &classes]);
+    Ok(())
+}
+
+/// Gets a teacher from the database
+pub fn get_teacher(conn: Connection, id: Id) -> Result<Teacher> {
+    let stmt = conn.prepare("SELECT * FROM teacher where id = ?1")?;
+    let teachers = stmt.query_map(&[&id], |row| {
+        // Parse from csv
+        let classes = getcsv(row.get(3));
+        if let Err(e) = classes {
+            return Err(e)
+        }
+        // Parse from json
+        Ok(Teacher {
+            id: row.get(0),
+            name: row.get(1),
+            password: row.get(2),
+            classes: classes.unwrap(),
+        })
+    })?;
+
+    if let Some(teacher) = teachers.next() {
+        if teachers.next().is_some() {
+            Err(anyhow!("Multiple teachers found with this id: {}", id))
+        } else {
+            Ok(teacher??)
+        }
+    } else {
+        Err(anyhow!("No teachers found with this id: {}", id))
+    }
 }
