5dca39028b66d3b08420a39cb84a568dca094438
capnhawkbill <capnhawkbill@airmail.cc>
2021-01-06 20:13:10 +0100

    get badge route for teacher and student

--- backend/src/routes/badge.rs
+++ backend/src/routes/badge.rs
@@ -15,21 +15,21 @@ use crate::{
     auth,
 };
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use rocket::Rocket;
 use rocket_contrib::json::Json;
 
 /// Mount all the routes
 pub fn mount(rocket: Rocket) -> Rocket {
-    rocket.mount("/api/badge", routes![award, create_badge, get_badge])
+    rocket.mount("/api/badge", routes![award, create_badge, get_badge_student, get_badge_teacher])
 }
 
 /// create a badge
 #[post("/create", format = "json", data = "<badge>")]
-pub fn create_badge(conn: DbConn, teacher: auth::Teacher, badge: Json<Badge>) -> Result<()> {
+pub fn create_badge(conn: DbConn, teacher: auth::Teacher, badge: Json<Badge>) -> Result<Id> {
     let id = generate_id(&*conn)?;
     let badge = models::Badge {
-        id,
+        id: id.clone(),
         name: badge.name.clone(),
         description: badge.description.clone(),
         official: false,
@@ -37,12 +37,25 @@ pub fn create_badge(conn: DbConn, teacher: auth::Teacher, badge: Json<Badge>) ->
 
     insert_badge(&*conn, &badge)?;
 
-    Ok(())
+    Ok(id)
 }
 
-/// get a badge
+/// get a badge student
 #[get("/get?<id>")]
-pub fn get_badge(conn: DbConn, id: Id) -> Result<Json<models::Badge>> {
+pub fn get_badge_student(conn: DbConn, student: auth::Student, id: Id) -> Result<Json<models::Badge>> {
+    if !(*student).badges.contains(&id) {
+        return Err(anyhow!("Student {:?} doesn't own badge {}", *student, id))
+    }
+    let badge = models::get_badge(&*conn, id)?;
+    Ok(Json(badge))
+}
+
+/// get a badge teacher
+#[get("/get?<id>", rank = 2)]
+pub fn get_badge_teacher(conn: DbConn, teacher: auth::Teacher, id: Id) -> Result<Json<models::Badge>> {
+    if !(*teacher).badges.contains(&id) {
+        return Err(anyhow!("Teacher {:?} doesn't own badge {}", *teacher, id))
+    }
     let badge = models::get_badge(&*conn, id)?;
     Ok(Json(badge))
 }
@@ -50,6 +63,7 @@ pub fn get_badge(conn: DbConn, id: Id) -> Result<Json<models::Badge>> {
 /// award a badge
 #[get("/award?<student>&<badge>")]
 pub fn award(conn: DbConn, teacher: auth::Teacher, student: Id, badge: Id) -> Result<()> {
+    // check if student is in a class of the teacher
     award_badge(&*conn, student, badge)?;
     Ok(())
 }
