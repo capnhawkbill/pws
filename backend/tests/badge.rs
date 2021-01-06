ece536ce6e4e25d70a68782cf5fae4d53c35b1e6
capnhawkbill <capnhawkbill@airmail.cc>
2021-01-06 20:13:22 +0100

    start with the badge test

---
+++ backend/tests/badge.rs
@@ -0,0 +1,84 @@
+extern crate backend;
+extern crate rocket;
+
+use backend::database::DbConn;
+use backend::testhelp::*;
+use backend::routes::*;
+
+use rocket::http::Header;
+use rocket::local::Client;
+
+const SIGNUPTEACHER: &'static str = r#"
+{
+    "username": "TestTeacher",
+    "password": "CoolPassword"
+}
+"#;
+const AUTHTEACHER: &'static str = "VGVzdFRlYWNoZXI6Q29vbFBhc3N3b3Jk";
+
+const SIGNUPSTUDENT: &'static str = r#"
+{
+    "username": "TestStudent",
+    "password": "CoolerPassword"
+}
+"#;
+const AUTHSTUDENT: &'static str = "VGVzdFN0dWRlbnQ6Q29vbGVyUGFzc3dvcmQ=";
+
+const BADGE: &'static str = r#"
+{
+    "name": "TestBadge",
+    "description": "A very cool badge for testing"
+}
+"#;
+
+#[test]
+fn test_badge() {
+    init_logger();
+    let config = init_test_db();
+
+    // Construct application
+    let rocket = rocket::custom(config).attach(DbConn::fairing());
+    let rocket = class::mount(rocket);
+    let rocket = student::mount(rocket);
+    let rocket = teacher::mount(rocket);
+    let rocket = badge::mount(rocket);
+
+    let client = Client::new(rocket).expect("Failed to initialize client");
+
+    let teacher = get_id_teacher(&client, SIGNUPTEACHER);
+    let student = get_id_student(&client, SIGNUPSTUDENT);
+
+    let authTeacher = Header::new("Authorization", AUTHTEACHER);
+    let class = client
+        .get("/api/class/create?name=testclass")
+        .header(authTeacher)
+        .dispatch()
+        .body_string()
+        .unwrap();
+
+    let authStudent = Header::new("Authorization", AUTHSTUDENT);
+    client
+        .get(format!("/api/class/join?id={}", class))
+        .header(authStudent)
+        .dispatch();
+
+    // create badge
+    let badge = client
+        .post("/api/badge/create")
+        .body(BADGE)
+        .header(authTeacher)
+        .header(ContentType::JSON)
+        .dispatch().body_string().unwrap();
+
+    // award the badge
+    client
+        .get(format!("/api/badge/award?student={}&badge={}", student, badge))
+        .header(authTeacher)
+        .dispatch();
+
+    let student_info = get_self_info_student(&client, AUTHSTUDENT);
+    let teacher_info = get_self_info_teacher(&client, AUTHTEACHER);
+
+    assert_eq!(student_info.badges, vec![badge]);
+    assert_eq!(teacher_info.badges, vec![badge]);
+}
