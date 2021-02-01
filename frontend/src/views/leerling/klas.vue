3f995aa4956ba22542630121231b30c7cbd2ba8a
Pomegranate123 <elias@hoste.info>
2021-02-01 18:30:18 +0100

    Add marking hw as done

--- frontend/src/views/leerling/klas.vue
+++ frontend/src/views/leerling/klas.vue
@@ -14,7 +14,8 @@
       <tbody>
         <tr v-for="homework in this.sortedhomework" v-bind:key="homework.date">
           <td v-for="(column, indexColumn) in this.homeworkcolumns" :key="indexColumn">{{homework[column]}}</td>
-          <td><a @click="toggleHomework(homework['id'])">Klaar?</a></td>
+          <td v-if="this.isDone(homework['id'])"><a @click="toggleHomework(homework['id'])">Niet klaar?</a></td>
+          <td v-else><a @click="toggleHomework(homework['id'])">Klaar?</a></td>
         </tr>
       </tbody>
     </table>
@@ -42,7 +43,7 @@ export default {
       klasvcolumns: ['Naam', 'Punten'],
       klascolumns: ['name', 'points'],
       klasleaderboard: null,
-      homeworkvcolumns: ['Titel', 'Datum', 'Beschrijving', 'Punten'],
+      homeworkvcolumns: ['Titel', 'Datum', 'Beschrijving', 'Punten', 'Acties'],
       homeworkcolumns: ['name', 'date', 'description', 'points'],
       homework: [],
       leerling: null,
@@ -50,64 +51,85 @@ export default {
     }
   },
   methods: {
+    isDone (homeworkid) {
+      if (this.leerling['homework'].indexOf(homeworkid) >= 0) {
+        return true
+      }
+      else {
+        return false
+      }
+    },
     toggleHomework (homeworkid) {
-      this.axios
-      .get('/api/homework/done?class=' + this.$route.params.id + '&homework=' + homeworkid, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
-      .then(() => {
-        console.log('Ok')
-      })
-      .catch(error => (console.log(error)))
-    }
-  },
-  mounted () { 
-    if (!this.$cookie.isCookieAvailable('student_auth')) {
-      this.$router.push({ name: 'leerling.login', query: { redirect: this.$route.fullPath}})
-    }
-    else {
-      const getHomework = (homeworkid) => {
+      if (this.isDone(homeworkid)) {
         this.axios
-        .get('/api/homework/get?id=' + homeworkid, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+        .get('/api/homework/undone?class=' + this.$route.params.id + '&homework=' + homeworkid, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+        .then(() => {
+          console.log('Ok')
+        })
+        .catch(error => (console.log(error)))
+      }
+      else {
+        this.axios
+        .get('/api/homework/done?class=' + this.$route.params.id + '&homework=' + homeworkid, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+        .then(() => {
+          console.log('Ok')
+        })
+        .catch(error => (console.log(error)))
+      }
+    },
+    reload () {
+      if (!this.$cookie.isCookieAvailable('student_auth')) {
+        this.$router.push({ name: 'leerling.login', query: { redirect: this.$route.fullPath}})
+      }
+      else {
+        const getHomework = (homeworkid) => {
+          this.axios
+          .get('/api/homework/get?id=' + homeworkid, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+          .then(response => {
+          const homework = response.data
+          this.homework.push(homework)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        }
+
+        this.axios
+        .get('/api/class/name?id=' + this.$route.params.id, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+        .then(response => (this.klasnaam = response.data))
+        .catch(error => {
+          console.log(error)
+        })
+        this.axios
+        .get('/api/class/leaderboard?class=' + this.$route.params.id, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+        .then(response => (this.klasleaderboard = response.data))
+        .catch(error => {
+          console.log(error)
+        })
+        this.axios
+        .get('/api/student/info', {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
+        .then(response => (this.leerling = response.data))
+        .catch(error => {
+          console.log(error)
+        })
+        this.axios
+        .get('/api/homework/get/class?id=' + this.$route.params.id, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
         .then(response => {
-        const homework = response.data
-        this.homework.push(homework)
+          for (let i = 0; i < response.data.length; i++) {
+            getHomework(response.data[i])
+          }
         })
         .catch(error => {
           console.log(error)
         })
+        .finally(() => {
+        this.loading = false
+        })
       }
-
-      this.axios
-      .get('/api/class/name?id=' + this.$route.params.id, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
-      .then(response => (this.klasnaam = response.data))
-      .catch(error => {
-        console.log(error)
-      })
-      this.axios
-      .get('/api/class/leaderboard?class=' + this.$route.params.id, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
-      .then(response => (this.klasleaderboard = response.data))
-      .catch(error => {
-        console.log(error)
-      })
-      this.axios
-      .get('/api/student/info', {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
-      .then(response => (this.leerling = response.data))
-      .catch(error => {
-        console.log(error)
-      })
-      this.axios
-      .get('/api/homework/get/class?id=' + this.$route.params.id, {'headers': {'Authorization': this.$cookie.getCookie('student_auth')}})
-      .then(response => {
-        for (let i = 0; i < response.data.length; i++) {
-          getHomework(response.data[i])
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-      .finally(() => {
-      this.loading = false
-      })
     }
+  },
+  mounted () { 
+    this.reload()
   },
   computed: {
     sortedhomework: function() {
